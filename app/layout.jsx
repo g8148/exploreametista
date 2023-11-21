34bd7f747305ec2ae81802f58548b2423a8d465a
Hayato8148 <gabriel8148mcpe@gmail.com>
2023-11-21 15:02:28 -0300

    updated dependencies

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,23 +3,23 @@ import "./globals.css";
 import { Noto_Sans } from "next/font/google";
 
 const noto_sans = Noto_Sans({
-    subsets: ["latin"],
-    weight: ["300", "400"],
+  subsets: ["latin"],
+  weight: ["300", "400"],
 });
 
 export const metadata = {
-    title: "Explore Ametista",
-    description:
-        "Explore Ametista - Um site com o objetivo de ajudar nossos visitantes!",
+  title: "Explore Ametista",
+  description:
+    "Explore Ametista - Um site com o objetivo de ajudar nossos visitantes!",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="pt-BR">
-            <body className={noto_sans.className}>
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pt-BR">
+      <body className={noto_sans.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
--- components/desc.jsx
+++ components/desc.jsx
@@ -2,51 +2,54 @@ import Slideshow from "@/components/slideshow";
 import Link from "next/link";
 
 export default function Desc({ título, fotos, mapa, mapalink, parag }) {
-  return (
-    <div>
-      <div className="flex justify-center mb-6">
-        <div className="flex w-[1200px] flex-col justify-center bg-[--background-corpo] h-auto p-8 mt-6 rounded-xl box-shadow2 md:p-0 md:py-8">
-          <h1 className="text-center mb-10 font-medium text-3xl">{título}</h1>
-          <div className="flex flex-col p-6 max-w-[900px] self-center text-center font-light md:p-3">
-            <h2 className="font-medium m-5 text-2xl">Fotos</h2>
+    return (
+        <div>
+            <div className="flex justify-center mb-6">
+                <div className="flex w-[1200px] flex-col justify-center bg-[--background-corpo] h-auto p-8 mt-6 rounded-xl box-shadow2 md:p-0 md:py-8">
+                    <h1 className="text-center mb-10 font-medium text-3xl">
+                        {título}
+                    </h1>
+                    <div className="flex flex-col p-6 max-w-[900px] self-center text-center font-light md:p-3">
+                        <h2 className="font-medium m-5 text-2xl">Fotos</h2>
 
-            <Slideshow imgs={fotos} />
+                        <Slideshow imgs={fotos} />
 
-            <h2 className="font-medium m-5 text-2xl">Descrição</h2>
-            <div className="text-justify rounded-xl px-4 indent-4 borderr">
-              {parag.map((texto) => (
-                <p className="my-4">{texto}</p>
-              ))}
-            </div>
+                        <h2 className="font-medium m-5 text-2xl">Descrição</h2>
+                        <div className="text-justify rounded-xl px-4 indent-4 borderr">
+                            {parag.map((texto) => (
+                                <p className="my-4">{texto}</p>
+                            ))}
+                        </div>
 
-            <div>
-              <h2 className="font-medium m-5 text-2xl">Mapa</h2>
+                        <div>
+                            <h2 className="font-medium m-5 text-2xl">Mapa</h2>
 
-              <span>
-                <div className="relative overflow-hidden w-full pt-[56.25%] z-[1]">
-                  <iframe
-                    className="absolute top-0 left-0 bottom-0 right-0 w-full h-full rounded-xl borderr"
-                    src={mapa}
-                    allowFullScreen=""
-                    referrerPolicy="no-referrer-when-downgrade"
-                    loading="lazy"
-                  ></iframe>
+                            <span>
+                                <div className="relative overflow-hidden w-full pt-[56.25%] z-[1]">
+                                    <iframe
+                                        className="absolute top-0 left-0 bottom-0 right-0 w-full h-full rounded-xl borderr"
+                                        src={mapa}
+                                        allowFullScreen=""
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                        loading="lazy"
+                                    ></iframe>
+                                </div>
+                                <br />
+                                Ou{" "}
+                                <Link
+                                    href={mapalink}
+                                    target="_blank"
+                                    className="no-underline text-[--black]"
+                                >
+                                    <strong>clique aqui</strong>
+                                </Link>{" "}
+                                para ser redirecionado automaticamente ao Google
+                                Maps
+                            </span>
+                        </div>
+                    </div>
                 </div>
-                <br />
-                Ou{" "}
-                <Link
-                  href={mapalink}
-                  target="_blank"
-                  className="no-underline text-[--black]"
-                >
-                  <strong>clique aqui</strong>
-                </Link>{" "}
-                para ser redirecionado automaticamente ao Google Maps
-              </span>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
